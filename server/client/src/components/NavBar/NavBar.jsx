e54b3de9f585c83007bcf33fff84a0c1c6c4527d
akulackovan <32873566+akulackovan@users.noreply.github.com>
2022-12-04 23:52:31 +0300

    Change seacrh from input to form

--- server/client/src/components/NavBar/NavBar.jsx
+++ server/client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import './NavBar.scss'
 
 const NavBar = () => {
@@ -7,14 +6,24 @@ const NavBar = () => {
         <nav>
             <div className="nav-wrapper navbar">
                 <a href='/' className="left photobank">ФОТОБАНК</a>
-                <ul className="center listHeader">
+                <ul className="listHeader">
                     <li><a href='/popular' className="listHeader">Популярное</a></li>
                     <li><a href='/subsc' className="listHeader">Подписки</a></li>
                     <li><a href='/profile' className="listHeader">Профиль</a></li>
                     <li><a href='/settings' className="listHeader">Настройки</a></li>
-                    <li><input className="search" placeholder="ПОИСК"></input></li>
-                </ul>
-            </div>
+                    <li>
+                        <form className="search" action="/search" method="get">
+                            <input
+                                type="text"
+                                id="header-search"
+                                placeholder="ПОИСК"
+                                name="s" 
+                            />
+                        </form>
+
+                        </li>
+                    </ul>
+                </div> 
         </nav>
     )
 }
