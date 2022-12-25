a67e96c1b6150d05d9a09c438fed5b3695bb4af1
akulackovan <32873566+akulackovan@users.noreply.github.com>
2022-12-25 22:57:20 +0300

    Add handler AuthPage

--- server/client/src/pages/AuthPage/AuthPage.jsx
+++ server/client/src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import { Link } from "react-router-dom"
+import axios from 'axios'
 import './AuthPage.scss'
+import { AuthContext } from '../../context/AuthContext'
 
 const AuthPage = () => {
+
+    const [form, setForm] = useState(
+        {
+            username: '',
+            password: ''
+        }
+    )
+
+    const changeForm = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+        console.log(form)
+    }
+
+    const { login } = useContext(AuthContext)
+
+
+    const authHandler = async () =>
+    {
+        try {
+            await axios.post('/auth/login', {...form}, {
+                headers:
+                {
+                    'Context-Type': 'application/json'
+                }
+            })
+            .then(response => 
+                login(response.data.token, response.data.user._id))
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className='auth'>
             <div className='center auth-page'>
@@ -10,19 +45,21 @@ const AuthPage = () => {
                 <input
                     className="login"
                     type="text"
-                    placeholder="Логин"
-                    name="user"
+                    placeholder="Логин" 
+                    name="username"
+                    onChange={changeForm}
                 />
                 <input
                     className="pass"
                     type="text"
                     placeholder="Пароль"
-                    name="user"
+                    name="password"
+                    onChange={changeForm}
                 />
-                <div className='regButton'>
-                    <button>ВОЙТИ</button>
-                    <Link to="/reg"><button>РЕГИСТРАЦИЯ</button></Link>
-                </div>
+                    <button
+                    onClick={authHandler}>ВОЙТИ</button>
+                    <button><Link to="/reg">РЕГИСТРАЦИЯ</Link></button>
+
             </div>
         </div>
     )
