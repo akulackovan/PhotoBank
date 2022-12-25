e2361b8475a35d3fff03a2728188a29064bef1ca
akulackovan <32873566+akulackovan@users.noreply.github.com>
2022-12-25 22:59:55 +0300

    Change App.js with routes

--- server/client/src/routes.js
+++ server/client/src/routes.js
@@ -1,32 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
-import NavBar from "./components/NavBar/NavBar";
-import AuthPage from "./pages/AuthPage/AuthPage";
-import PopularPage from './pages/PopularPage/PopularPage'
-import RegPage from "./pages/RegPage/RegPage";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import "./App.scss";
+import {useRoutes} from './routes'
+import {AuthContext} from './context/AuthContext'
+import {useAuth} from './hooks/auth.hook'
 
-export const useRoutes = (isLogin) =>
-{
-    if (isLogin)
-    {
-        console.log(isLogin)
-        return (
-            <div className="app">
-                    <NavBar />
-                    <Router>
-                        <Switch>
-                            <Route exact path='/popular' component={PopularPage} />
-                            <Redirect to='/popular' />
-                        </Switch>
-                    </Router>
-                
-            </div>
-        )
-    }
-    return (
-        <Switch>
-            <Route exact path='/reg' component={RegPage} />
-            <Route exact path='/auth' component={AuthPage} />
-        </Switch>
-    )
+function App() {
+  const {login, logout, token, userId, isReady} = useAuth()
+  const isLogin = !!token
+  const routes = useRoutes(isLogin)
+  console.log(token)
+  return (
+    <AuthContext.Provider value= {{login, logout, token, userId, isReady, isLogin}}>
+      <div className="app">
+        <Router>
+            { routes }
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
 }
+
+export default App;
