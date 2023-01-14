2cf207039a344da275caa20d988c766fe561c5e3
ivan-gorshechnikov <vania.gorshevhnikov@gmail.com>
2023-01-14 18:57:20 +0300

    Update controllers

--- server/controllers/getPostById.js
+++ server/controllers/getPostById.js
@@ -4,14 +4,17 @@ import jwt from 'jsonwebtoken'
 
 export const getPostById = async (req, res) => {
     try {
-        const { id } = req.body
-        const isPost = await Post.findById({ id })
+        const {id} = req.body
+        const isPost = await Post.findById({id})
         if (isPost) {
             return isPost
         }
-        return res.json({ message: 'Поста не существует.' })
-    }
-    catch (error) {
-        res.json({ message: 'Ошибка при получении поста.' })
+        return res.status(400).json({
+            message: 'Поста не существует'
+        })
+    } catch (error) {
+        return res.status(400).json({
+            message: 'Ошибка при получении поста'
+        })
     }
 }
--- server/controllers/getPostByUserId.js
+++ server/controllers/getPostByUserId.js
@@ -4,20 +4,18 @@ import jwt from 'jsonwebtoken'
 
 export const getPostByUserId = async (req, res) => {
     try {
-        const { idUser } = req.body
-        const { options } = {
+        const {idUser} = req.body
+        const {options} = {
             projection: {_id: 0, username: 0, password: 0, city: 0, image: 0, posts: 1, subscriptions: 0}
         }
-        const isUser = await User.findById({ idUser }, { options })
+        const isUser = await User.findById({idUser}, {options})
 
-        if (!isUser) {
-            return res.json({ message: 'Пользователя не существует.' })
+        if (isUser) {
+            return isUser
         }
+        return res.status(400).json({message: 'Пользователя не существует'})
 
-        return isUser       
- 
-    }
-    catch (error) {
-        res.json({ message: 'Ошибка при получении постов пользователя.' })
+    } catch (error) {
+        return res.status(400).json({message: 'Ошибка при получении постов пользователя'})
     }
 }
