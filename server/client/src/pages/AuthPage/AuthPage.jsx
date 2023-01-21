5163a925123caafd918c4b5988cb51e257561ff9
ivan-gorshechnikov <61047586+ivan-gorshechnikov@users.noreply.github.com>
2023-01-21 05:38:57 +0300

    Get popular post front (#12)
    * Add navbar
    
    * Add client folder
    
    * Change seacrh from input to form
    
    * Change NavBav
    
    * Add AuthPage without css
    
    * Add AuthPage without css
    
    * Add scss to Authpage
    
    * Add handler AuthPage
    
    * Add routes.js
    
    * Add hooks
    
    * Add context
    
    * Change App.js with routes
    
    * Add settings with password
    
    * Add settings for city and description
    
    * Add setting for login
    
    * Add autorefresh
    
    * Go to popular if login
    
    * Change to user update
    
    * Change css and js AuthPage
    
    * Update NavBar
    
    * Fix style, add last line and update routing
    
    * Fix style
    
    * Fix style
    
    * Fix style
    
    * Настройки пользователя front (#6)
    
    * Add SettingPage
    
    * Update settings page
    
    * Add last lines
    
    * Fix style
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    
    * Регистрация front (#4)
    
    * Registration page
    
    * Change name from Auth to Peg
    
    * Move to folder RegPage
    
    * Update registration page
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    
    * Получение постов пользователя backend (#2)
    
    * Add model post
    
    * Get post by id
    
    * Get posts by user's id
    
    * Update controllers
    
    * Add last line
    
    * Fix path to Post.js
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    
    * Регистрация и авторизация пользователя backend (#7)
    
    * add libs & create config
    
    * create schema "user"
    
    * create utility
    
    * create function for authorization
    
    * add mongodb and dotenv
    
    * add schemas to db
    
    * create routers
    
    * add routers checking auth & HTTP-status
    
    * add "post" in routers
    
    * Fix style
    
    * Fix style
    
    Co-authored-by: kubi1 <kubi1-ya@ya.ru>
    
    * Получение постов подписчиков, популярных постов, создание и удаление поста backend (#1)
    
    * create post managing functions
    
    * feature getPopularPosts and getSubscriptionPostContent
    
    * Remove temp files from idea
    
    * Add new line at end
    
    * исправление выявленных недочетов
    
    * исправление выявленных недочетов
    
    * получение города юзера и сортировка
    
    * функция подписки
    
    Co-authored-by: danilpiatov <dan.pyatoff2011@yandex.ru>
    
    * Add front of popular pages
    
    * Fix dev of getting popular post
    
    * Make table better
    
    * Final fix
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    Co-authored-by: kubi1 <kubi1-ya@ya.ru>
    Co-authored-by: danilpiatov <dan.pyatoff2011@yandex.ru>

---
+++ server/client/src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,93 @@
+import React, {useState, useContext} from 'react'
+import {Redirect} from "react-router-dom"
+import axios from 'axios'
+import './AuthPage.scss'
+import {AuthContext} from '../../context/AuthContext'
+
+const AuthPage = () => {
+
+    const [form, setForm] = useState(
+        {
+            username: '',
+            password: ''
+        }
+    )
+    const [errorMessage, setErrorMessage] = React.useState("");
+    const [redirect, setRedirect] = React.useState(false);
+    const [token, setToken] = React.useState("");
+    const [userId, setUserID] = React.useState("");
+    const [authRed, setAuthRed] = React.useState(false);
+
+    const changeForm = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+        console.log(form)
+    }
+
+    const {login} = useContext(AuthContext)
+
+
+    const authHandler = async () => {
+        try {
+            await axios.post('/auth/login', {...form}, {
+                headers:
+                    {
+                        'Context-Type': 'application/json'
+                    }
+            })
+                .then(response => {
+                    setAuthRed(true)
+
+                    console.log(authRed)
+                    console.log("authRed")
+                    console.log("RES")
+                    setToken(response.data.token)
+                    setUserID(response.data.user._id)
+                })
+        } catch (error) {
+            console.log(error)
+            setErrorMessage(error.response.data.message)
+        }
+    }
+
+    const handleOnClick = async () => {
+        setRedirect(true)
+    }
+    console.log(authRed)
+    console.log("authRed")
+    if (authRed) {
+        login(token, userId)
+        return (
+            <Redirect to='/popular'/>
+        )
+    }
+
+    return (
+        <div className='auth'>
+            <div className='center back'>
+                <h3 className="head">ФОТОБАНК</h3>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Логин"
+                    name="username"
+                    onChange={changeForm}
+                />
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Пароль"
+                    name="password"
+                    onChange={changeForm}
+                />
+                <button className='button'
+                        onClick={authHandler}>ВОЙТИ
+                </button>
+                <button className='button' onClick={handleOnClick}>РЕГИСТРАЦИЯ</button>
+                {errorMessage && <div className="error"> {errorMessage} </div>}
+                {redirect && <Redirect to='/reg'/>}
+            </div>
+        </div>
+    )
+}
+
+export default AuthPage
---
+++ server/router/auth.js
@@ -0,0 +1,15 @@
+import {Router} from "express"
+import {getMe, login, register} from '../controllers/auth.js'
+import {checkAuth} from "../utils/checkAuth.js";
+
+const router = new Router()
+
+router.post('/reg', register)
+
+router.post('/login', login)
+
+router.get('/profile', getMe)
+
+router.post('/subscribe', subscibe)
+
+export default router
