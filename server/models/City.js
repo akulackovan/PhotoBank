c97c834ce650efb17f4546696b3e3ff754ab3a15
ivan-gorshechnikov <61047586+ivan-gorshechnikov@users.noreply.github.com>
2023-01-21 06:50:13 +0300

    Revert "Hot fix get popular post (#16)"
    This reverts commit c34b8441c2d6de0e3b734a8cd9462646c703cbfc.

--- server/models/City.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const CitySchema = new mongoose.Schema(
-    {
-        city: {type: String, required: true},
-    },
-)
-
-export default mongoose.model('City', CitySchema)
