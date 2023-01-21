c34b8441c2d6de0e3b734a8cd9462646c703cbfc
ivan-gorshechnikov <61047586+ivan-gorshechnikov@users.noreply.github.com>
2023-01-21 06:49:26 +0300

    Hot fix get popular post (#16)
    * Add navbar
    
    * Add client folder
    
    * Change seacrh from input to form
    
    * Change NavBav
    
    * Add AuthPage without css
    
    * Add AuthPage without css
    
    * Add scss to Authpage
    
    * Add handler AuthPage
    
    * Add routes.js
    
    * Add hooks
    
    * Add context
    
    * Change App.js with routes
    
    * Add settings with password
    
    * Add settings for city and description
    
    * Add setting for login
    
    * Add autorefresh
    
    * Go to popular if login
    
    * Change to user update
    
    * Change css and js AuthPage
    
    * Update NavBar
    
    * Fix style, add last line and update routing
    
    * Fix style
    
    * Fix style
    
    * Fix style
    
    * Настройки пользователя front (#6)
    
    * Add SettingPage
    
    * Update settings page
    
    * Add last lines
    
    * Fix style
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    
    * Регистрация front (#4)
    
    * Registration page
    
    * Change name from Auth to Peg
    
    * Move to folder RegPage
    
    * Update registration page
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    
    * Получение постов пользователя backend (#2)
    
    * Add model post
    
    * Get post by id
    
    * Get posts by user's id
    
    * Update controllers
    
    * Add last line
    
    * Fix path to Post.js
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    
    * Регистрация и авторизация пользователя backend (#7)
    
    * add libs & create config
    
    * create schema "user"
    
    * create utility
    
    * create function for authorization
    
    * add mongodb and dotenv
    
    * add schemas to db
    
    * create routers
    
    * add routers checking auth & HTTP-status
    
    * add "post" in routers
    
    * Fix style
    
    * Fix style
    
    Co-authored-by: kubi1 <kubi1-ya@ya.ru>
    
    * Получение постов подписчиков, популярных постов, создание и удаление поста backend (#1)
    
    * create post managing functions
    
    * feature getPopularPosts and getSubscriptionPostContent
    
    * Remove temp files from idea
    
    * Add new line at end
    
    * исправление выявленных недочетов
    
    * исправление выявленных недочетов
    
    * получение города юзера и сортировка
    
    * функция подписки
    
    Co-authored-by: danilpiatov <dan.pyatoff2011@yandex.ru>
    
    * Add front of popular pages
    
    * Fix dev of getting popular post
    
    * Make table better
    
    * Final fix
    
    * Update today photos
    
    Co-authored-by: akulackovan <32873566+akulackovan@users.noreply.github.com>
    Co-authored-by: kubi1 <kubi1-ya@ya.ru>
    Co-authored-by: danilpiatov <dan.pyatoff2011@yandex.ru>

---
+++ server/controllers/postController.js
@@ -0,0 +1,43 @@
+import Post from "../models/Post.js";
+
+export const createPost = async (req, res) => {
+    try {
+        const {city, image, text} = req.body;
+        if (!city || !image || !text) {
+            return res.status(400).json({
+                message: 'Ошибка при создании поста',
+            });
+        }
+        const newPost = new Post({
+            author: req.authenticatorData.userId,
+            city: city,
+            image: image,
+            text: text,
+            view: 0,
+            likes: 0,
+        });
+        await newPost.save();
+        res.status(201).json({
+            newPost,
+            message: 'Успешно созданный пост',
+        });
+    } catch {
+        res.status(400).json({message: 'Ошибка при создании поста'});
+    }
+};
+
+export const deletePost = async (req, res) => {
+    try {
+        const {postId} = req.body;
+        const post = await Post.findOne({postId});
+        if (!post) {
+            return res.status(404).json({
+                message: 'Такого поста не существует',
+            });
+        }
+        await Post.deleteOne(postId);
+        res.status(204).json({message: 'Пост был удален'});
+    } catch {
+        res.status(400).json({message: 'Ошибка при удалении поста'});
+    }
+};
